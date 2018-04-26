321af188edf5541256237ea052ab50730e54f5b8
Javier Munoz Sastre <javier.munoz@metrica.es>
2018-04-26 15:59:50 +0200

    Añado el proyecto 4

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+const path = require('path');
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/src/index.html'));
+});
